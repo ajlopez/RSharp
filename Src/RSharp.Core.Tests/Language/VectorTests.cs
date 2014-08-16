607e3260f7fe205a7f724d6e4106764425608575
ajlopez <webmaster@ajlopez.com>
2014-08-16 14:55:44 -0300

    Vector first test

---
+++ Src/RSharp.Core.Tests/Language/VectorTests.cs
@@ -0,0 +1,24 @@
+﻿namespace RSharp.Core.Tests.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RSharp.Core.Language;
+
+    [TestClass]
+    public class VectorTests
+    {
+        [TestMethod]
+        public void CreateVectorWithIntegers()
+        {
+            Vector v = new Vector(new object[] { 1, 2, 3 });
+
+            Assert.AreEqual(3, v.Length);
+            Assert.AreEqual(1, v[0]);
+            Assert.AreEqual(2, v[1]);
+            Assert.AreEqual(3, v[2]);
+        }
+    }
+}
---
+++ Src/RSharp.Core/Language/Vector.cs
@@ -0,0 +1,27 @@
+﻿namespace RSharp.Core.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Vector
+    {
+        private IList<object> elements;
+
+        public Vector(IEnumerable<object> elements)
+        {
+            this.elements = new List<object>(elements);
+        }
+
+        public int Length { get { return this.elements.Count; } }
+
+        public object this[int index] 
+        {
+            get
+            {
+                return this.elements[index];
+            }
+        }
+    }
+}
