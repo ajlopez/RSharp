9cfb687e38087e180fa03cd7dce7a744e55f5f0a
ajlopez <webmaster@ajlopez.com>
2014-08-16 19:10:56 -0300

    Parse call expression

--- Src/RSharp.Core.Tests/Interpreter/ParserTests.cs
+++ Src/RSharp.Core.Tests/Interpreter/ParserTests.cs
@@ -1,12 +1,14 @@
 ﻿namespace RSharp.Core.Tests.Interpreter
 {
     using System;
-    using System.Text;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Text;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using RSharp.Core.Interpreter;
     using RSharp.Core.Expressions;
+    using RSharp.Core.Functions;
+    using RSharp.Core.Interpreter;
+    using RSharp.Core.Language;
 
     [TestClass]
     public class ParserTests
@@ -33,6 +35,51 @@
             IsNameExpression(expr, "foo");
 
             Assert.IsNull(parser.ParseExpression());
+        }
+
+        [TestMethod]
+        public void ParseAndEvaluateCallExpression()
+        {
+            var parser = new Parser("c(1,2,3)");
+
+            var expr = parser.ParseExpression();
+
+            Assert.IsNotNull(expr);
+            Assert.IsInstanceOfType(expr, typeof(CallExpression));
+
+            Context context = new Context();
+            context.SetValue("c", new MakeVector());
+
+            var result = expr.Evaluate(context);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Vector));
+
+            var vector = (Vector)result;
+
+            Assert.AreEqual(3, vector.Length);
+            Assert.AreEqual(1, vector[0]);
+            Assert.AreEqual(2, vector[1]);
+            Assert.AreEqual(3, vector[2]);
+
+            Assert.IsNull(parser.ParseExpression());
+        }
+
+        [TestMethod]
+        public void RaiseExpectedComma()
+        {
+            var parser = new Parser("c(1 2 3)");
+
+            try
+            {
+                parser.ParseExpression();
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(ParserException));
+                Assert.AreEqual("Expected ','", ex.Message);
+            }
         }
 
         private static void IsConstantExpression(IExpression expr, object value)
--- Src/RSharp.Core/Interpreter/Parser.cs
+++ Src/RSharp.Core/Interpreter/Parser.cs
@@ -11,6 +11,7 @@
     public class Parser
     {
         private Lexer lexer;
+        private Stack<Token> tokens = new Stack<Token>();
 
         public Parser(string text)
             : this(new StringReader(text))
@@ -29,6 +30,29 @@
 
         public IExpression ParseExpression()
         {
+            IExpression expr = this.ParseTerm();
+
+            if (expr == null)
+                return null;
+
+            if (!this.TryNextToken(TokenType.Delimiter, "("))
+                return expr;
+
+            IList<IExpression> exprs = new List<IExpression>();
+
+            while (!this.TryNextToken(TokenType.Delimiter, ")"))
+            {
+                if (exprs.Count > 0)
+                    this.NextToken(TokenType.Delimiter, ",");
+
+                exprs.Add(this.ParseExpression());
+            }
+
+            return new CallExpression(expr, exprs);
+        }
+
+        private IExpression ParseTerm()
+        {
             var token = this.NextToken();
 
             if (token == null)
@@ -42,7 +66,35 @@
 
         private Token NextToken()
         {
+            if (this.tokens.Count > 0)
+                return this.tokens.Pop();
+
             return this.lexer.NextToken();
+        }
+
+        private void PushToken(Token token)
+        {
+            this.tokens.Push(token);
+        }
+
+        private void NextToken(TokenType type, string value)
+        {
+            var token = this.NextToken();
+
+            if (token == null || token.Type != type || token.Value != value)
+                throw new ParserException(string.Format("Expected '{0}'", value));
+        }
+
+        private bool TryNextToken(TokenType type, string value)
+        {
+            var token = this.NextToken();
+
+            if (token != null && token.Type == type && token.Value == value)
+                return true;
+
+            this.PushToken(token);
+
+            return false;
         }
     }
 }
