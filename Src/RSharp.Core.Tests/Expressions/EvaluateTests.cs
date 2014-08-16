8512716549817755a652a36a61641bbcdf40b4c7
ajlopez <webmaster@ajlopez.com>
2014-08-16 18:16:52 -0300

    Expression evaluate first tests

---
+++ Src/RSharp.Core.Tests/Expressions/EvaluateTests.cs
@@ -0,0 +1,33 @@
+﻿namespace RSharp.Core.Tests.Expressions
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RSharp.Core.Expressions;
+
+    [TestClass]
+    public class EvaluateTests
+    {
+        [TestMethod]
+        public void EvaluateConstantExpression()
+        {
+            Assert.AreEqual(1, (new ConstantExpression(1)).Evaluate(null));
+            Assert.AreEqual("foo", (new ConstantExpression("foo")).Evaluate(null));
+        }
+
+        [TestMethod]
+        public void EvaluateNameExpression()
+        {
+            Context context = new Context();
+
+            context.SetValue("one", 1);
+            context.SetValue("name", "Adam");
+
+            Assert.AreEqual(1, (new NameExpression("one")).Evaluate(context));
+            Assert.AreEqual("Adam", (new NameExpression("name")).Evaluate(context));
+            Assert.IsNull((new NameExpression("foo")).Evaluate(context));
+        }
+    }
+}
