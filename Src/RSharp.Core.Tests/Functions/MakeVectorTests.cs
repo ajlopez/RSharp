c45e29ce4fa0c7741e768b8c30af8fd80f4da9cd
ajlopez <webmaster@ajlopez.com>
2014-08-16 18:49:14 -0300

    Function MakeVector first test

---
+++ Src/RSharp.Core.Tests/Functions/MakeVectorTests.cs
@@ -0,0 +1,32 @@
+﻿namespace RSharp.Core.Tests.Functions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RSharp.Core.Functions;
+    using RSharp.Core.Language;
+
+    [TestClass]
+    public class MakeVectorTests
+    {
+        [TestMethod]
+        public void MakeVectorWithThreeIntegers()
+        {
+            MakeVector fn = new MakeVector();
+
+            var result = fn.Apply(new object[] { 1, 2, 3 });
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Vector));
+
+            var vector = (Vector)result;
+
+            Assert.AreEqual(3, vector.Length);
+            Assert.AreEqual(1, vector[0]);
+            Assert.AreEqual(2, vector[1]);
+            Assert.AreEqual(3, vector[2]);
+        }
+    }
+}
---
+++ Src/RSharp.Core/Functions/MakeVector.cs
@@ -0,0 +1,16 @@
+﻿namespace RSharp.Core.Functions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using RSharp.Core.Language;
+
+    public class MakeVector
+    {
+        public object Apply(IList<object> arguments)
+        {
+            return new Vector(arguments);
+        }
+    }
+}
