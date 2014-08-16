fee86355dd2b65d192ac782058622b404a02a11c
ajlopez <webmaster@ajlopez.com>
2014-08-16 16:53:22 -0300

    Additional test, lexer get integer with spaces

--- Src/RSharp.Core.Tests/Interpreter/LexerTests.cs
+++ Src/RSharp.Core.Tests/Interpreter/LexerTests.cs
@@ -49,6 +49,19 @@
             Assert.IsNull(lexer.NextToken());
         }
 
+        [TestMethod]
+        public void GetIntegerWithSpaces()
+        {
+            Lexer lexer = new Lexer("   42  ");
+
+            var token = lexer.NextToken();
+
+            Assert.IsNotNull(token);
+            IsToken(token, TokenType.Integer, "42");
+
+            Assert.IsNull(lexer.NextToken());
+        }
+
         private static void IsToken(Token token, TokenType type, string value)
         {
             Assert.AreEqual(token.Value, value);
