f9bc4601202167c6ce80ee64ad73992c91452495
ajlopez <webmaster@ajlopez.com>
2014-08-16 18:12:37 -0300

    Context first tests

---
+++ Src/RSharp.Core.Tests/ContextTests.cs
@@ -0,0 +1,30 @@
+﻿namespace RSharp.Core.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ContextTests
+    {
+        [TestMethod]
+        public void GetUndefinedValueAsNull()
+        {
+            var context = new Context();
+
+            Assert.IsNull(context.GetValue("foo"));
+        }
+
+        [TestMethod]
+        public void SetAndGetValue()
+        {
+            var context = new Context();
+
+            context.SetValue("one", 1);
+
+            Assert.AreEqual(1, context.GetValue("one"));
+        }
+    }
+}
---
+++ Src/RSharp.Core/Context.cs
@@ -0,0 +1,25 @@
+﻿namespace RSharp.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Context
+    {
+        private IDictionary<string, object> values = new Dictionary<string, object>();
+
+        public void SetValue(string name, object value)
+        {
+            this.values[name] = value;
+        }
+
+        public object GetValue(string name)
+        {
+            if (this.values.ContainsKey(name))
+                return this.values[name];
+
+            return null;
+        }
+    }
+}
