20518cd6827be4984552f7235b7b0db1705740dc
ajlopez <webmaster@ajlopez.com>
2014-08-18 07:11:01 -0300

    Machine evaluate first test

---
+++ Src/RSharp.Core.Tests/MachineTests.cs
@@ -0,0 +1,35 @@
+﻿namespace RSharp.Core.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RSharp.Core.Expressions;
+    using RSharp.Core.Interpreter;
+    using RSharp.Core.Language;
+
+    [TestClass]
+    public class MachineTests
+    {
+        [TestMethod]
+        public void EvaluateMakeVector()
+        {
+            Machine machine = new Machine();
+            Parser parser = new Parser("c(1,2,3)");
+            IExpression expr = parser.ParseExpression();
+
+            var result = machine.Evaluate(expr);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Vector));
+
+            var vector = (Vector)result;
+
+            Assert.AreEqual(3, vector.Length);
+            Assert.AreEqual(1, vector[0]);
+            Assert.AreEqual(2, vector[1]);
+            Assert.AreEqual(3, vector[2]);
+        }
+    }
+}
---
+++ Src/RSharp.Core/Machine.cs
@@ -0,0 +1,25 @@
+﻿namespace RSharp.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using RSharp.Core.Functions;
+    using RSharp.Core.Expressions;
+
+    public class Machine
+    {
+        private Context rootcontext;
+
+        public Machine()
+        {
+            this.rootcontext = new Context();
+            this.rootcontext.SetValue("c", new MakeVector());
+        }
+
+        public object Evaluate(IExpression expr)
+        {
+            return expr.Evaluate(this.rootcontext);
+        }
+    }
+}
