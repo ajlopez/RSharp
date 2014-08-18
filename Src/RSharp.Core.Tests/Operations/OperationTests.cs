5dd3142c9a3ea952ce7d9cc7ed4ad3283b9e73a0
ajlopez <webmaster@ajlopez.com>
2014-08-18 08:01:24 -0300

    Add vector to numbers

--- Src/RSharp.Core.Tests/Operations/OperationTests.cs
+++ Src/RSharp.Core.Tests/Operations/OperationTests.cs
@@ -6,6 +6,7 @@
     using System.Linq;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using RSharp.Core.Operations;
+    using RSharp.Core.Language;
 
     [TestClass]
     public class OperationTests
@@ -19,6 +20,82 @@
             Assert.AreEqual(1.2 + 3.4, op.Apply(1.2, 3.4));
             Assert.AreEqual(1 + 2.3, op.Apply(1, 2.3));
             Assert.AreEqual(1.2 + 3, op.Apply(1.2, 3));
+        }
+
+        [TestMethod]
+        public void AddVectorToInteger()
+        {
+            AddOperation op = new AddOperation();
+            Vector v = new Vector(new object[] { 1, 2, 3 });
+
+            var result = op.Apply(v, 1);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Vector));
+
+            var vector = (Vector)result;
+
+            Assert.AreEqual(3, vector.Length);
+            Assert.AreEqual(2, vector[0]);
+            Assert.AreEqual(3, vector[1]);
+            Assert.AreEqual(4, vector[2]);
+        }
+
+        [TestMethod]
+        public void AddIntegerToVector()
+        {
+            AddOperation op = new AddOperation();
+            Vector v = new Vector(new object[] { 1, 2, 3 });
+
+            var result = op.Apply(1, v);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Vector));
+
+            var vector = (Vector)result;
+
+            Assert.AreEqual(3, vector.Length);
+            Assert.AreEqual(2, vector[0]);
+            Assert.AreEqual(3, vector[1]);
+            Assert.AreEqual(4, vector[2]);
+        }
+
+        [TestMethod]
+        public void AddVectorToReal()
+        {
+            AddOperation op = new AddOperation();
+            Vector v = new Vector(new object[] { 1, 2, 3 });
+
+            var result = op.Apply(v, 1.5);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Vector));
+
+            var vector = (Vector)result;
+
+            Assert.AreEqual(3, vector.Length);
+            Assert.AreEqual(1 + 1.5, vector[0]);
+            Assert.AreEqual(2 + 1.5, vector[1]);
+            Assert.AreEqual(3 + 1.5, vector[2]);
+        }
+
+        [TestMethod]
+        public void AddRealToVector()
+        {
+            AddOperation op = new AddOperation();
+            Vector v = new Vector(new object[] { 1, 2, 3 });
+
+            var result = op.Apply(1.5, v);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Vector));
+
+            var vector = (Vector)result;
+
+            Assert.AreEqual(3, vector.Length);
+            Assert.AreEqual(1 + 1.5, vector[0]);
+            Assert.AreEqual(2 + 1.5, vector[1]);
+            Assert.AreEqual(3 + 1.5, vector[2]);
         }
 
         [TestMethod]
--- Src/RSharp.Core/Operations/AddOperation.cs
+++ Src/RSharp.Core/Operations/AddOperation.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using RSharp.Core.Language;
 
     public class AddOperation
     {
@@ -12,13 +13,19 @@
             if (left is int)
                 if (right is int)
                     return (int)left + (int)right;
-                else
+                else if (right is double)
                     return (int)left + (double)right;
-            else
+                else
+                    return ((Vector)right).Add(left);
+            else if (left is double)
                 if (right is int)
                     return (double)left + (int)right;
-                else
+                else if (right is double)
                     return (double)left + (double)right;
+                else
+                    return ((Vector)right).Add(left);
+            else
+                return ((Vector)left).Add(right);
         }
     }
 }
