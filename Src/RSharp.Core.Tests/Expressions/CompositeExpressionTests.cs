dee56d23a4bb91c871d1d85a3358794e5e580633
ajlopez <webmaster@ajlopez.com>
2017-04-03 12:08:40 -0300

    CompositeExpression first naive evaluation test

---
+++ Src/RSharp.Core.Tests/Expressions/CompositeExpressionTests.cs
@@ -0,0 +1,19 @@
+﻿namespace RSharp.Core.Tests.Expressions
+{
+    using System;
+    using System.Collections;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RSharp.Core.Expressions;
+
+    [TestClass]
+    public class CompositeExpressionTests
+    {
+        [TestMethod]
+        public void EvaluateCompositeWithoutReturnReturnsNull()
+        {
+            CompositeExpression expression = new CompositeExpression(new IExpression[] { });
+
+            Assert.IsNull(expression.Evaluate(null));
+        }
+    }
+}
--- Src/RSharp.Core/Expressions/CompositeExpression.cs
+++ Src/RSharp.Core/Expressions/CompositeExpression.cs
@@ -18,7 +18,7 @@
 
         public object Evaluate(Context context)
         {
-            throw new NotImplementedException();
+            return null;
         }
     }
 }
