9a9d53917a5c9f47066c8301ddde9fa678668efa
ajlopez <webmaster@ajlopez.com>
2017-04-15 10:03:26 -0300

    ArrayAccessExpression

---
+++ Src/RSharp.Core.Tests/Expressions/ArrayAccessExpressionTests.cs
@@ -0,0 +1,26 @@
+﻿namespace RSharp.Core.Tests.Expressions
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RSharp.Core.Expressions;
+    using RSharp.Core.Functions;
+    using RSharp.Core.Language;
+
+    [TestClass]
+    public class ArrayAccessExpressionTests
+    {
+        [TestMethod]
+        public void GetSecondElement()
+        {
+            Vector vector = new Vector(new object[] { 1, 2, 3 });
+            IExpression arrexpr = new ConstantExpression(vector);
+            IExpression indexpr = new ConstantExpression(1);
+
+            IExpression expr = new ArrayAccessExpression(arrexpr, new IExpression[] { indexpr });
+
+            Assert.AreEqual(2, expr.Evaluate(null));
+        }
+    }
+}
---
+++ Src/RSharp.Core/Expressions/ArrayAccessExpression.cs
@@ -0,0 +1,33 @@
+﻿namespace RSharp.Core.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using RSharp.Core.Functions;
+    using RSharp.Core.Language;
+
+    public class ArrayAccessExpression : IExpression
+    {
+        private IExpression arrexpr;
+        private IList<IExpression> argexprs;
+
+        public ArrayAccessExpression(IExpression fn, IList<IExpression> argexprs)
+        {
+            this.arrexpr = fn;
+            this.argexprs = argexprs;
+        }
+
+        public IExpression ArrayExpression { get { return this.arrexpr; } }
+
+        public IList<IExpression> ArgumentExpressions { get { return this.argexprs; } }
+
+        public object Evaluate(Context context)
+        {
+            Vector vector = (Vector)this.arrexpr.Evaluate(context);
+            IList<object> args = new List<object>();
+
+            return vector[(int)this.argexprs[0].Evaluate(context)];
+        }
+    }
+}
