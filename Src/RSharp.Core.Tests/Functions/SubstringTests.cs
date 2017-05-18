3db497b7ff39267cebb6c3667a67a4fcacb92640
ajlopez <webmaster@ajlopez.com>
2017-05-18 17:27:00 -0300

    Substring function, WIP

---
+++ Src/RSharp.Core.Tests/Functions/SubstringTests.cs
@@ -0,0 +1,21 @@
+﻿namespace RSharp.Core.Tests.Functions
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RSharp.Core.Functions;
+
+    [TestClass]
+    public class SybstringTests
+    {
+        [TestMethod]
+        public void SubstringTest()
+        {
+            var fn = new Substring();
+
+            var result = fn.Apply(null, new object[] { "Equator", 3, 5 });
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("uat", result);
+        }
+    }
+}
---
+++ Src/RSharp.Core/Functions/Substring.cs
@@ -0,0 +1,15 @@
+﻿namespace RSharp.Core.Functions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Substring : IFunction
+    {
+        public object Apply(Context context, IList<object> args)
+        {
+            return null;
+        }
+    }
+}
