0f8092fbbb89b149d875d88db0db628b3482f15d
ajlopez <webmaster@ajlopez.com>
2017-05-25 14:42:48 -0300

    NChar as function

---
+++ Src/RSharp.Core.Tests/Functions/NCharTests.cs
@@ -0,0 +1,22 @@
+﻿namespace RSharp.Core.Tests.Functions
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RSharp.Core.Functions;
+    using RSharp.Core.Language;
+
+    [TestClass]
+    public class NCharTests
+    {
+        [TestMethod]
+        public void NCharOfAnString()
+        {
+            NChar fn = new NChar();
+
+            var result = fn.Apply(null, new object[] { "hello" });
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(5, result);
+        }
+    }
+}
---
+++ Src/RSharp.Core/Functions/NChar.cs
@@ -0,0 +1,18 @@
+﻿namespace RSharp.Core.Functions
+{
+    using RSharp.Core.Language;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class NChar : IFunction
+    {
+        public object Apply(Context context, IList<object> args)
+        {
+            var arg = args[0];
+
+            return ((String)arg).Length;
+        }
+    }
+}
