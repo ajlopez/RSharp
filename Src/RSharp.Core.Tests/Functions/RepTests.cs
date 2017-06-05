48c541f18e2c35b0e3dbd6be131ea34772b46437
ajlopez <webmaster@ajlopez.com>
2017-06-05 19:37:16 -0300

    Rep first implementation and test

---
+++ Src/RSharp.Core.Tests/Functions/RepTests.cs
@@ -0,0 +1,51 @@
+﻿namespace RSharp.Core.Tests.Functions
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RSharp.Core.Functions;
+    using RSharp.Core.Language;
+
+    [TestClass]
+    public class RepTests
+    {
+        [TestMethod]
+        public void RepTwice()
+        {
+            var fn = new Rep();
+
+            var result = fn.Apply(null, new object[] { "foo", 2 }, null);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Vector));
+
+            var vector = (Vector)result;
+
+            Assert.AreEqual(2, vector.Length);
+            Assert.AreEqual("foo", vector[0]);
+            Assert.AreEqual("foo", vector[0]);
+        }
+
+        [TestMethod]
+        public void PasteTwoStrings()
+        {
+            var fn = new Paste();
+
+            var result = fn.Apply(null, new object[] { "foo", "bar" }, null);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("foo bar", result);
+        }
+
+        [TestMethod]
+        public void PasteTwoStringsUsingEmptySeparator()
+        {
+            var fn = new Paste();
+
+            var result = fn.Apply(null, new object[] { "foo", "bar" }, new Dictionary<string, object>() { { "sep",  string.Empty } });
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("foobar", result);
+        }
+    }
+}
---
+++ Src/RSharp.Core/Functions/Rep.cs
@@ -0,0 +1,23 @@
+﻿namespace RSharp.Core.Functions
+{
+    using RSharp.Core.Language;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Rep : IFunction
+    {
+        public object Apply(Context context, IList<object> args, IDictionary<string, object> namedvalues)
+        {
+            var value = args[0];
+            var ntimes = (int)args[1];
+            IList<object> values = new List<object>();
+
+            for (int k = 0; k < ntimes; k++)
+                values.Add(value);
+
+            return new Vector(values);
+        }
+    }
+}
