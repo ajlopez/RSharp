1c200eab2cf834781e00131c42476937fca66b39
ajlopez <webmaster@ajlopez.com>
2017-06-08 10:48:00 -0300

    Min function first implementation and test

---
+++ Src/RSharp.Core.Tests/Functions/MinTests.cs
@@ -0,0 +1,33 @@
+﻿namespace RSharp.Core.Tests.Functions
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RSharp.Core.Functions;
+    using RSharp.Core.Language;
+
+    [TestClass]
+    public class MinTests
+    {
+        [TestMethod]
+        public void MinOfTwoIntegers()
+        {
+            IFunction fn = new Min();
+
+            var result = fn.Apply(null, new object[] { 1, 42 }, null);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void MinOfThreeIntegers()
+        {
+            IFunction fn = new Min();
+
+            var result = fn.Apply(null, new object[] { 42, 1, 2 }, null);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result);
+        }
+    }
+}
---
+++ Src/RSharp.Core/Functions/Min.cs
@@ -0,0 +1,29 @@
+﻿namespace RSharp.Core.Functions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Min : IFunction
+    {
+        public object Apply(Context context, IList<object> values, IDictionary<string, object> namedvalues)
+        {
+            int? result = null;
+
+            foreach (var value in values)
+            {
+                if (value is int)
+                    if (result.HasValue)
+                    {
+                        if (result > (int)value)
+                            result = (int)value;
+                    }
+                    else
+                        result = (int)value;
+            }
+
+            return result;
+        }
+    }
+}
