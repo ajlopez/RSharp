f8436a147564e93e8bf537ccd29b1e5f0fadbf6e
ajlopez <webmaster@ajlopez.com>
2017-06-17 10:27:27 -0300

    Fixing StyleCop

--- Src/RSharp.Core/Functions/Rep.cs
+++ Src/RSharp.Core/Functions/Rep.cs
@@ -1,10 +1,10 @@
 ﻿namespace RSharp.Core.Functions
 {
-    using RSharp.Core.Language;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using RSharp.Core.Language;
 
     public class Rep : IFunction
     {
